d78d0976e34529823195c48e5f22701e5d601c03
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-05-16 13:02:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassCounter counter = new ClassCounter();
+            Handler_1 handler_1 = new Handler_1();
+            Handler_2 handler_2 = new Handler_2();
+
+            counter.Count();
+            counter.onCount += handler_1.Message;
+            counter.onCount += handler_2.Message;
+            counter.onCount += counter.Message;
+            counter.Count();
+            Console.Read();
+        }
+    }
+}
