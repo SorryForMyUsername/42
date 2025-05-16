18649af8cbbccf8dd411cf5d502e52071f58914d
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-05-16 13:02:33 +0300

    Add files via upload

---
+++ Books.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Books
+    {
+        protected List<string> BookNames = new List<string>();
+
+        public delegate void BookHandler(String text);
+        public event BookHandler onAddBook;
+        public event BookHandler onDeleteBook;
+        public void AddBook(string bookName)
+        {
+            BookNames.Add(bookName);
+            if(onAddBook != null)
+            {
+                onAddBook("Книга " +  bookName + " добавлена.");
+            }
+        }
+
+        public void DeleteBook(string bookName)
+        {
+            BookNames.Remove(bookName);
+            if(onDeleteBook != null)
+            {
+                onDeleteBook("Книга " + bookName + " УДАЛЕНА!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books books = new Books();
+            books.onAddBook += ShowMessage;
+            books.onDeleteBook += ShowMessage;
+            books.AddBook("Властелин колец");
+            books.AddBook("Звёздные войны");
+            books.DeleteBook("Звёздные войны");
+        }
+
+        public static void ShowMessage(String text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
